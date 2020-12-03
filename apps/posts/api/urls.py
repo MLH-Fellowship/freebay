a2eee8a5d65425c1bb3553fefc189243327dada4
Nyior Clement Jr <cnyior27@gmail.com>
2020-12-03 23:59:26 +0100

    one view that handles post, get(single and list) requests. isn't that magical? it's django baby xD

--- apps/posts/api/urls.py
+++ apps/posts/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from apps.posts.api import views as av
 
+router = DefaultRouter()
+router.register(r"items", av.ItemsViewSet)
 
 urlpatterns = [
-    
+    path("", include(router.urls)),    
 ]
--- freebay_backend/urls.py
+++ freebay_backend/urls.py
@@ -16,6 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('admin/', admin.site.urls), 
+    path('api/v1/', include('apps.posts.api.urls')),
+    path('api-auth', include('rest_framework.urls'))
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
